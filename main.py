458169e1856551960a7073291441b224cbeb6083
D0Nater <53537581+D0Nater@users.noreply.github.com>
2020-11-10 16:28:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -392,11 +392,12 @@ class MoreInfoInterface:
 
         # Artist #
         self.song_artist_draw = self.song_info_canvas.create_text(40, 80, text=languages['Артист'][settings.language]+':', fill=themes[settings.theme]['text_color'], anchor=W, font="Verdana 13")
+        self.song_info_canvas.create_text(self.song_info_canvas.bbox(self.song_artist_draw)[2]+11, self.song_info_canvas.bbox(self.song_artist_draw)[1]+3, text=data[1], fill='cyan', anchor=NW, font="Verdana 12")
 
         # Song size #
         self.song_size_draw = self.song_info_canvas.create_text(40, 110, text=languages['Размер'][settings.language]+':', fill=themes[settings.theme]['text_color'], anchor=W, font="Verdana 13")
 
-        # Song time #
+        # Song duration #
         self.song_duration_draw = self.song_info_canvas.create_text(40, 140, text=languages['Длительность'][settings.language]+':', fill=themes[settings.theme]['text_color'], anchor=W, font="Verdana 13")
 
         root.update()
@@ -404,24 +405,20 @@ class MoreInfoInterface:
         # Search data #
         self.song_data = Music.more_song_info(data[5])
 
-        # Loading Artist #
-        self.song_info_canvas.create_window(self.song_info_canvas.bbox(self.song_artist_draw)[2]+9, 81, window=Button(text=data[1], bd=0, fg='cyan', bg=themes[settings.theme]['second_color'], activebackground=themes[settings.theme]['second_color'], activeforeground=themes[settings.theme]['text_color'], font="Verdana 12"), anchor=W)
+        # Draw Song size #
+        self.song_info_canvas.create_text(self.song_info_canvas.bbox(self.song_size_draw)[2]+11, self.song_info_canvas.bbox(self.song_size_draw)[1]+3, text=self.song_data['size']+' mb', fill=themes[settings.theme]['text_color'], anchor=NW, font="Verdana 12")
 
-        # Loading Song size #
-        self.song_info_canvas.create_text(self.song_info_canvas.bbox(self.song_size_draw)[2]+15, self.song_info_canvas.bbox(self.song_size_draw)[1]+3, text=self.song_data['size']+' mb', fill=themes[settings.theme]['text_color'], anchor=NW, font="Verdana 12")
+        # Draw Song duration #
+        self.song_info_canvas.create_text(self.song_info_canvas.bbox(self.song_duration_draw)[2]+11, self.song_info_canvas.bbox(self.song_duration_draw)[1]+2, text=data[3], fill=themes[settings.theme]['text_color'], anchor=NW, font="Verdana 12")
 
-        # Loading Song duration #
-        self.song_info_canvas.create_text(self.song_info_canvas.bbox(self.song_duration_draw)[2]+15, self.song_info_canvas.bbox(self.song_duration_draw)[1]+2, text=data[3], fill=themes[settings.theme]['text_color'], anchor=NW, font="Verdana 12")
+        # Draw Song text #
+        self.song_info_canvas.create_text(40, 180, text=languages['Текст'][settings.language]+':', fill=themes[settings.theme]['text_color'], anchor=W, font="Verdana 13")
 
-        # Loading Song text #
-        if self.song_data['text'] != '':
-            self.song_info_canvas.create_text(40, 180, text=languages['Текст'][settings.language]+':', fill=themes[settings.theme]['text_color'], anchor=W, font="Verdana 13")
-
-            self.song_text_draw = Text(width=37, height=22, bg=themes[settings.theme]['second_color'], fg=themes[settings.theme]['text_color'], bd=1, wrap=WORD, font="Verdana 12")
-            self.song_text_draw.insert(END, self.song_data['text']) # write text in block
-            self.song_text_draw.config(state=DISABLED) # update config
-
-            self.song_info_canvas.create_window(40, 200, window=self.song_text_draw, anchor=NW)
+        # Draw Block for text #
+        self.song_text_draw = Text(width=37, height=22, bg=themes[settings.theme]['second_color'], fg=themes[settings.theme]['text_color'], bd=1, wrap=WORD, font="Verdana 12")
+        self.song_text_draw.insert(END, self.song_data['text']) # write text in block
+        self.song_text_draw.config(state=DISABLED) # update config
+        self.song_info_canvas.create_window(40, 200, window=self.song_text_draw, anchor=NW)
 
         globals()['scroll_win'] = False
 
