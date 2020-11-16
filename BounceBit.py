1dd958c433089b0e1a177ec7333a6ed30da59c29
D0Nater <53537581+D0Nater@users.noreply.github.com>
2020-11-16 22:03:19 +0300

    Add files via upload

---
+++ BounceBit.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+"""
+      : READ_ME :
+     : BounceBit :     
+: Open Source Project :
+
+  ~ Language: Python ~
+ ~ Interface: Tkinter ~
+~ Media player: Pyglet ~
+
+     - Author:  D0Nater -
+- GitHub: github.com/D0Nater -
+"""
+
+""" For Interface """
+from tkinter import *
+
+""" For icon """
+from os import path
+
+""" For parse news """
+from threading import Thread
+from Scripts.elements import *
+
+""" For Player """
+from Scripts.my_player import MyPlayer
+
+""" For key events """
+from Scripts.key_event import KeyEvent
+
+""" For news """
+from Scripts.news import News
+
+""" For load Images """
+from Scripts.images import LoadPictures
+
+""" Settings """
+from Scripts.settings import Settings
+
+""" Interface """
+from Scripts.menu_interface import Menu
+from Scripts.window_for_data import WindowForData
+from Scripts.song_line import SongLine
+
+""" Main """
+from Scripts.main import Main
+
+
+class BounceBit:
+    def __init__(self):
+        # Parse News #
+        Thread(target=News.parse_new_news).start()
+
+        # Start Program #
+        Main.ROOT = Tk()
+        Main.ROOT.title("")
+
+        # Settings #
+        Main.SETTINGS = Settings("en", "dark")
+        Main.SETTINGS.update_settings()
+
+        # Window settings #
+        Main.ROOT.iconbitmap(default=LoadPictures.resource_path(path.join("pictures", "program_icon.ico")))
+        Main.ROOT.geometry(f"{Main.SETTINGS.width-50}x{Main.SETTINGS.height-100}+10+10")
+        Main.ROOT.minsize(width=180, height=45)
+        Main.ROOT.state("zoomed")
+
+        # Player #
+        Main.PLAYER = MyPlayer()
+
+        # Load images #
+        Main.LOAD_IMAGE = LoadPictures()
+        Main.LOAD_IMAGE.upd_images()
+
+        # Draw menu #
+        Main.MENU = Menu()
+        Main.MENU.draw_menu()
+        Main.MENU.update_buttons()
+
+        # Draw main canvas for data #
+        Main.WINDOW_FOR_DATA = WindowForData()
+        Main.WINDOW_FOR_DATA.drow_data()
+
+        # Draw line for song #
+        Main.SONG_LINE = SongLine()
+
+        # Keys settings #
+        KeyEvent()
+
+        # Start window #
+        Main.ROOT.mainloop()
+
+
+if __name__ == "__main__":
+    BounceBit()
