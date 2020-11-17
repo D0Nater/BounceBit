2fa73204ac761fce13d7c0064b2529be6bf783d9
D0Nater <53537581+D0Nater@users.noreply.github.com>
2020-11-17 14:35:49 +0300

    Update main.py

--- Scripts/main.py
+++ Scripts/main.py
@@ -3,6 +3,7 @@
 
 class Main:
     LIST_OF_IDS = []
+    SONG_TIME_NOW = "00:00"
     LIST_OF_PLAY = {"classes": []}
     PAST_SONG = {"class": None, "song_id": None, "past_lib": None, "lib_now": None}
     SONG_PLAY_NOW = {"play": 0, "name": "", "author": "", "time": "", "url": "", "song_id": None, "num": 0, "loaded": False}
