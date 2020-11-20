9ab1845e63d9473dee7875e983ac5395d45a2192
D0Nater <53537581+D0Nater@users.noreply.github.com>
2020-11-20 15:24:24 +0300

    Update parse_music.py

--- Scripts/parse_music.py
+++ Scripts/parse_music.py
@@ -3,21 +3,16 @@
 """ For databases """
 import sqlite3
 
-""" For music in playlists """
-import json
-
-""" For files """
-from os import path, mkdir, remove
-
 """ For clear RAM """
 from gc import collect as clear_ram
-
-""" For download music """
-from mutagen.easyid3 import EasyID3
 
 """ For Parse """
 import requests
 import lxml.html
+from json import loads as json_loads
+
+""" Other Scripts """
+from Scripts.elements import *
 
 
 def parse_data(url):
@@ -30,171 +25,95 @@ def parse_data(url):
         raise ConnectionError
 
 
-def music_pages(tree, page, xpath_text):
-    # Pages #
-    pages_music_json = []
-    try:
-        # page now (default 1) #
-        pages_music_json.append(page)
-
-        # other pages #
-        for num in range((2 if int(page) > 1 else 1), 6):
-            try:
-                pages_music_json.append(tree.xpath(xpath_text % num)[0])
-            except:
-                pass
-
-        # delete last button #
-        if pages_music_json[-1] == "Следующая":
-            (pages_music_json).pop()
-
-    except:
-        pass
-    
-    # sorted and return pages #
-    return sorted(pages_music_json)
-
-
-top_music_json = {"music": {"num": 0}, "url": "", "error": None}
-
-
 class ParseMusic:
-    def top_music():
-        clear_ram()
-        global top_music_json
+    @staticmethod
+    def music_pages(tree, page):
+        # Pages #
+        pages_array = []
         try:
-            if top_music_json["music"]["num"] is 0:
-                top_music_json = {"music": {"num": 0}, "error": None}
-                
-                # parse site #
-                tree = parse_data("https://zaycev.net/")
+            pages_array.append(page)
 
-                # music #
-                for num in range(1, 61):
-                    try:
-                        new_song = {
-                            "name": tree.xpath(f'//*[@id="top_1"]/div[2]/div[{num}]/div[1]/div[2]/div[3]/a/text()')[0],
-                            "author": tree.xpath(f'//*[@id="top_1"]/div[2]/div[{num}]/div[1]/div[2]/div[1]/a/text()')[0],
-                            "url": tree.xpath(f'//*[@id="top_1"]/div[2]/div[{num}]/@data-url')[0].split('/')[-2],
-                            "data_key": tree.xpath(f'//*[@id="top_1"]/div[2]/div[{num}]/@data-dkey')[0],
-                            "song_time": tree.xpath(f'//*[@id="top_1"]/div[2]/div[{num}]/div[2]/text()')[0],
-                            "song_id": tree.xpath(f'//*[@id="top_1"]/div[2]/div[{num}]/@data-id')[0]
-                        }
-                        top_music_json["music"][f"song{num-1}"] = new_song
-                        top_music_json["music"]["num"] += 1
-                        del new_song
+            # other pages #
+            page_num = (3 if page > 2 else 1)
+            for num in range(0, 4):
+                try:
+                    pages_array.append(int(tree.xpath(f'//*[@id="pjax-container"]/div/div/section/ul/li[{page_num}]/a/text()')[0]))
+                except:
+                    pass
 
-                    except KeyError:
-                        break
+                page_num += 1
 
-                top_music_json["error"] = None
+        except:
+            pass
+        
+        # sorted and return pages #
+        return sorted(pages_array)
 
-                del tree
-
-        except ConnectionError:
-            top_music_json["error"] = "connect_error"
-
-        return top_music_json
-
-    def search_music(text, page):
+    @staticmethod
+    def search_data(url, page=1):
         clear_ram()
-        search_music_json = {"music": {"num": 0}, "pages": [], "error": None}
+        search_data_json = {"music": {"num": 0}, "pages": [], "error": None}
         try:
             # parse site #
-            tree = parse_data(f'https://zaycev.net/search.html?page={page}&query_search={text}')
+            tree = parse_data(url)
 
             # music #
-            song_num = 0
-            for num in range(1, 41):
+            for num in range(1, 49):
                 try:
-                    if tree.xpath(f'//*[@id="search-results"]/div/div[3]/div/div[1]/div[1]/div[2]/div[{num}]/@class')[0].split(' ')[-1] != "track-is-banned":
-                        new_song = {
-                            "name": tree.xpath(f'//*[@id="search-results"]/div/div[3]/div/div[1]/div[1]/div[2]/div[{num}]/div[1]/div[2]/div[3]/a/text()')[0],
-                            "author": tree.xpath(f'//*[@id="search-results"]/div/div[3]/div/div[1]/div[1]/div[2]/div[{num}]/div[1]/div[2]/div[1]/a/text()')[0],
-                            "url": tree.xpath(f'//*[@id="search-results"]/div/div[3]/div/div[1]/div[1]/div[2]/div[{num}]/@data-url')[0].split('/')[-2],
-                            "data_key": tree.xpath(f'//*[@id="search-results"]/div/div[3]/div/div[1]/div[1]/div[2]/div[{num}]/@data-dkey')[0],
-                            "song_time": tree.xpath(f'//*[@id="search-results"]/div/div[3]/div/div[1]/div[1]/div[2]/div[{num}]/div[2]/text()')[0],
-                            "song_id": tree.xpath(f'//*[@id="search-results"]/div/div[3]/div/div[1]/div[1]/div[2]/div[{num}]/@data-id')[0]
-                        }
-                        search_music_json["music"][f"song{song_num}"] = new_song
-                        search_music_json["music"]["num"] += 1
-                        del new_song
-                        song_num += 1
-                    else:
-                        pass
+                    new_song = json_loads(tree.xpath(f'//*[@id="pjax-container"]/div/div/ul[@class="tracks__list"]/li[{num}]/@data-musmeta')[0])
 
-                except KeyError:
+                    new_song = {
+                        "name": new_song["title"],
+                        "author": new_song["artist"],
+                        "url": new_song["url"],
+                        "song_time": tree.xpath(f'//*[@id="pjax-container"]/div/div/ul[@class="tracks__list"]/li[{num}]/div[3]/div/div/div[1]/text()')[0],
+                        "song_id": new_song["id"].split("-")[-1]
+                    }
+
+                    search_data_json["music"][f"song{num-1}"] = new_song
+                    search_data_json["music"]["num"] += 1
+
+                    del new_song
+
+                except (KeyError, IndexError):
                     break
 
-            # Pages #
-            search_music_json["pages"] = music_pages(tree, page, '//*[@id="search-page"]/div/div/div[3]/div/a[%s]/span/text()')
+            search_data_json["pages"] = ParseMusic.music_pages(tree, page)
 
-            search_music_json["error"] = None
+            search_data_json["error"] = None
 
             del tree
 
         except ConnectionError:
-            search_music_json["error"] = "connect_error"
+            search_data_json["error"] = "connect_error"
 
-        return search_music_json
+        return search_data_json
 
+    @staticmethod
+    def top_music(page):
+        page_str = "/start/" + str(48*(page-1)) if page > 1 else ""
+        return ParseMusic.search_data(f"http://m.hitmos.com/songs/top-today{page_str}", page)
+
+    @staticmethod
+    def search_music(search_text, page):
+        page_str = "/start/" + str(48*(page-1)) if page > 1 else ""
+        return ParseMusic.search_data(f"http://m.hitmos.com/search{page_str}?q={search_text}", page)
+
+    @staticmethod
     def genres_music(genre, page):
+        page_str = "/start/" + str(48*(page-1)) if page > 1 else ""
+        return ParseMusic.search_data("http://m.hitmos.com/genre/%s%s"%(languages[genre]["num"], page_str), page)
+
+    @staticmethod
+    def more_song_info(song_id):
         clear_ram()
-        genre_music_json = {"music": {"num": 0}, "pages": [], "error": None}
+        more_song_info_json = {"size": "", "error": None}
         try:
             # parse site #
-            tree = parse_data(f'https://zaycev.net/genres/{genre}/index_{page}.html?spa=false&page={page}')
-
-            # music #
-            song_num = 0
-            for num in range(1, 41):
-                try:
-                    if tree.xpath(f'//*[@id="genre-tracks"]/div[2]/div[1]/div[{num}]/@class')[0].split(' ')[-1] != "track-is-banned":
-                        new_song = {
-                            "name": tree.xpath(f'//*[@id="genre-tracks"]/div[2]/div[1]/div[{num}]/div[1]/div[2]/div[3]/a/text()')[0],
-                            "author": tree.xpath(f'//*[@id="genre-tracks"]/div[2]/div[1]/div[{num}]/div[1]/div[2]/div[1]/a/text()')[0],
-                            "url": tree.xpath(f'//*[@id="genre-tracks"]/div[2]/div[1]/div[{num}]/@data-url')[0].split('/')[-2],
-                            "data_key": tree.xpath(f'//*[@id="genre-tracks"]/div[2]/div[1]/div[{num}]/@data-dkey')[0],
-                            "song_time": tree.xpath(f'//*[@id="genre-tracks"]/div[2]/div[1]/div[{num}]/div[2]/text()')[0],
-                            "song_id": tree.xpath(f'//*[@id="genre-tracks"]/div[2]/div[1]/div[{num}]/@data-id')[0]
-                        }
-                        genre_music_json["music"][f"song{song_num}"] = new_song
-                        genre_music_json["music"]["num"] += 1
-                        del new_song
-                        song_num += 1
-                    else:
-                        pass
-
-                except KeyError:
-                    break
-
-            # Pages #
-            genre_music_json["pages"] = music_pages(tree, page, '//*[@id="page-body"]/div[2]/div/div[2]/div/div/div/section/div[@class="pager clearfix"]/a[%s]/span/text()')
-
-            genre_music_json["error"] = None
-
-            del tree
-
-        except ConnectionError:
-            genre_music_json["error"] = "connect_error"
-
-        return genre_music_json
-
-    def more_song_info(data_key):
-        clear_ram()
-        more_song_info_json = {"size": "", "text": "", "error": None}
-        try:
-            data_key = data_key.split(".")[0]
-
-            # parse site #
-            tree = parse_data(f'https://zaycev.net/pages{data_key}.shtml')
+            tree = parse_data(f"http://m.hitmos.com/song/{song_id}")
 
             # Song size #
-            more_song_info_json["size"] = tree.xpath('//*[@id="audiotrack-info"]/div[1]/div[2]/p[1]/text()')[0].split(" ")[1]
-
-            # Song text #
-            try: more_song_info_json["text"] = tree.xpath('//*[@id="audiotrack-block"]/div[3]/div[2]/div/div[@class="audiotrack-lyrics__text text-expander__text"]/text()')[0]
-            except: pass
+            more_song_info_json["size"] = tree.xpath('//*[@id="pjax-container"]/div[1]/div/div[2]/div/select/option[2]/text()')[0].split(" ")[-2]
 
         except ConnectionError:
             more_song_info_json["error"] = "connect_error"
