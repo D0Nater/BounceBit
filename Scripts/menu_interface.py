ab1029f30d0f7192b1ba39eab7ddb8ebe5b11a72
D0Nater <53537581+D0Nater@users.noreply.github.com>
2020-11-20 15:27:24 +0300

    Update menu_interface.py

--- Scripts/menu_interface.py
+++ Scripts/menu_interface.py
@@ -23,8 +23,8 @@ class Menu(MusicInterface):
 
     def draw_menu(self):
         Main.MENU_CANVAS = Canvas(Main.ROOT, width=Main.SETTINGS.width, height=77, bg=themes[Main.SETTINGS.theme]["second_color"], highlightthickness=0)
-        Main.MENU_CANVAS.create_text(10, 25, text=PROGRAM_NAME, fill="red", anchor=W, font="Aharoni 20") # program name
-        Main.MENU_CANVAS.create_text(145, 27, text=f"v{VERSION}", anchor=W, fill="red") # version
+        Main.PROGRAM_NAME_DRAW = Main.MENU_CANVAS.create_text(10, 25, text=PROGRAM_NAME, fill="red", anchor=W, font="Aharoni 20") # program name
+        Main.VERSION_DRAW = Main.MENU_CANVAS.create_text(145, 27, text=f"v{VERSION}", anchor=W, fill="red") # version
         Main.MENU_CANVAS.pack()
 
     def update_buttons(self):
@@ -35,7 +35,7 @@ class Menu(MusicInterface):
             command=lambda: self.music_interface("Избранное", MusicStorage.read_music("database2.sqlite", "add_error"))).place(x=181, y=53)
 
         self.recommended_music_button = Button(text=languages["Рекомендации"][Main.SETTINGS.language], bg=themes[Main.SETTINGS.theme]["second_color"], fg=themes[Main.SETTINGS.theme]["text_color"], bd=1, width=25, \
-            command=lambda: self.music_interface("Рекомендации", None)).place(x=363, y=53)
+            command=lambda: self.music_interface("Рекомендации 1", None)).place(x=363, y=53)
 
         self.settings_button = Button(text=languages["Настройки"][Main.SETTINGS.language], bg=themes[Main.SETTINGS.theme]["second_color"], fg=themes[Main.SETTINGS.theme]["text_color"], bd=1, width=25, \
             command=lambda: Main.SETTINGS_INTERFACE.settings_interface()).place(x=544, y=53)
