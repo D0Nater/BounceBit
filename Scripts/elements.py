4913f7b9c13f1836bf6b4b10b7ed3fff15466ae4
D0Nater <53537581+D0Nater@users.noreply.github.com>
2020-11-21 19:00:56 +0300

    Update elements.py

--- Scripts/elements.py
+++ Scripts/elements.py
@@ -14,7 +14,7 @@ def clear_list_of_songs():
 
 
 """ Elements for main program """
-VERSION = "0.4"
+VERSION = "0.4.1"
 AUTHOR = "D0Nater"
 GITHUB = "https://github.com/D0Nater/BounceBit/"
 PROGRAM_NAME = "Bounce Bit"
