5343a54365e301f44e326d0a7ed25f3cb76d76b1
D0Nater <53537581+D0Nater@users.noreply.github.com>
2020-11-22 23:15:10 +0300

    Update my_player.py

--- Scripts/my_player.py
+++ Scripts/my_player.py
@@ -3,6 +3,7 @@
 """ For play music """
 import pyglet
 import pyglet.media as media
+from time import time as time_time
 from time import sleep as time_sleep
 
 
@@ -11,17 +12,26 @@ class MyPlayer:
         self.player = pyglet.media.Player()
         self.play_song_while = False
 
+        self._time = 0.0
+        self._systime = None
+
     def new_song(self, song_id):
         self.player.queue(media.load(f"Databases/Download_Music/{song_id}.mp3"))
 
-    def jump(self, time):
-        self.player.seek(time)
+    def get_song_time(self):
+        return self.get_time()
+
+    def set_time(self, new_time):
+        self.reset()
+        self._time = new_time
+        self.player.seek(new_time)
 
     def next_song(self):
         self.player.next_source()
 
     def play(self):
         self.play_song_while = True
+        self._systime = time_time()
         while self.play_song_while:
             self.player.play()
             time_sleep(0.4)
@@ -31,6 +41,23 @@ class MyPlayer:
         self.player.pause()
         self.play_song_while = False
 
+        self._time = self.get_time()
+        self._systime = None
+
     def stop(self):
         self.pause()
         self.player.delete()
+
+    def reset(self):
+        """Reset the timer to 0."""
+        self._time = 0.0
+        if self._systime is not None:
+            self._systime = time_time()
+
+    def get_time(self):
+        """Get the elapsed time."""
+        if self._systime is None:
+            now = self._time
+        else:
+            now = time_time() - self._systime + self._time
+        return now
