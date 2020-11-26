17541a14b5872965db02ab026cd57bf1914cef15
D0Nater <53537581+D0Nater@users.noreply.github.com>
2020-11-26 13:59:29 +0300

    Update update_program.py

--- Scripts/update_program.py
+++ Scripts/update_program.py
@@ -2,13 +2,19 @@
 
 """ For Interface """
 from tkinter import *
-from tkinter import messagebox
 
 """ For Parse """
+import json
+import requests
 import lxml.html
+
+""" For download update """
+from threading import Thread
 
 """ Other """
 from Scripts.elements import *
+
+""" For parse data """
 from Scripts.parse_music import parse_data
 
 """ Images """
@@ -43,22 +49,38 @@ class UpdateProgram:
 
             # button 'download' #
             self.download_button = Main.MENU_CANVAS.create_window(Main.MENU_CANVAS.bbox(self.update_text_draw)[2]+15, Main.MENU_CANVAS.bbox(self.update_text_draw)[3]-10, anchor=W, window=Button(image=MyImage.DOWNLOAD_UPD, width=18, height=20, bd=0, bg=themes[Main.SETTINGS.theme]["second_color"], activebackground=themes[Main.SETTINGS.theme]["second_color"], relief=RIDGE, \
-                command=lambda: self.download_upd()))
+                command=lambda: Thread(target=self.download_upd)))
 
             # button 'close' #
             self.close_button = Main.MENU_CANVAS.create_window(Main.MENU_CANVAS.bbox(self.download_button)[2]+12, Main.MENU_CANVAS.bbox(self.update_text_draw)[3]-7, anchor=W, window=Button(image=MyImage.CLOSE, width=16, height=16, bd=0, bg=themes[Main.SETTINGS.theme]["second_color"], activebackground=themes[Main.SETTINGS.theme]["second_color"], relief=RIDGE, \
-                command=lambda: self.close_upd()))
+                command=lambda: self.close_msg()))
+
+    def get_download_url(self):
+        # parse site #
+        tree = parse_data("https://github.com/D0Nater/BounceBit/blob/main/DownloadProgram")
+
+        # parse and decode text #
+        return tree.xpath(f'//*[@id="LC1"]/text()')[0]
 
     def download_upd(self):
-        messagebox.showinfo("BounceBit Update", "Download function is testing!")
+
+        self.__google_file__ = self.get_download_url()
+
+        with open(f"BounceBitInstall_{self.version}.exe", "wb") as f:
+            response = requests.get(self.__google_file__, stream=True)
+
+            for data in response.iter_content(chunk_size=4096):
+                f.write(data)
+
+        self.close_msg()
 
     def update_msg(self):
         if self.draw_update:
-            self.close_upd()
+            self.close_msg()
             self.draw_update = True
             self.create_text_upd()
 
-    def close_upd(self):
+    def close_msg(self):
         Main.MENU_CANVAS.delete(self.update_text_draw)
         Main.MENU_CANVAS.delete(self.download_button)
         Main.MENU_CANVAS.delete(self.close_button)
