c5b379d64b1df8df9cc93a0bc37144ce157f503a
D0Nater <53537581+D0Nater@users.noreply.github.com>
2020-12-05 21:33:15 +0300

    Update update_program.py

--- Scripts/update_program.py
+++ Scripts/update_program.py
@@ -45,14 +45,14 @@ class UpdateProgram:
     def create_text_upd(self):
         if self.draw_update:
             # update text #
-            self.update_text_draw = Main.MENU_CANVAS.create_text(Main.MENU_CANVAS.bbox(Main.VERSION_DRAW)[2]+20, 25, text=languages["update_text"][Main.SETTINGS.language]+" \""+self.version+"\" !", anchor=W, fill=themes[Main.SETTINGS.theme]["text_color"], font="Verdana 11")
+            self.update_text_draw = Main.MENU_CANVAS.create_text(Main.MENU_CANVAS.bbox(Main.VERSION_DRAW)[2]+20, 27, text=languages["update_text"][Main.SETTINGS.language]+" \""+self.version+"\" !", anchor=W, fill=themes[Main.SETTINGS.theme]["text_color"], font="Verdana 11")
 
             # button 'download' #
-            self.download_button = Main.MENU_CANVAS.create_window(Main.MENU_CANVAS.bbox(self.update_text_draw)[2]+15, Main.MENU_CANVAS.bbox(self.update_text_draw)[3]-10, anchor=W, window=Button(image=MyImage.DOWNLOAD_UPD, width=18, height=20, bd=0, bg=themes[Main.SETTINGS.theme]["second_color"], activebackground=themes[Main.SETTINGS.theme]["second_color"], relief=RIDGE, \
+            self.download_button = Main.MENU_CANVAS.create_window(Main.MENU_CANVAS.bbox(self.update_text_draw)[2]+15, Main.MENU_CANVAS.bbox(self.update_text_draw)[3]-12, anchor=W, window=Button(image=MyImage.DOWNLOAD_UPD, width=18, height=20, bd=0, bg=themes[Main.SETTINGS.theme]["second_color"], activebackground=themes[Main.SETTINGS.theme]["second_color"], relief=RIDGE, \
                 command=lambda: Thread(target=self.download_upd)))
 
             # button 'close' #
-            self.close_button = Main.MENU_CANVAS.create_window(Main.MENU_CANVAS.bbox(self.download_button)[2]+12, Main.MENU_CANVAS.bbox(self.update_text_draw)[3]-7, anchor=W, window=Button(image=MyImage.CLOSE, width=16, height=16, bd=0, bg=themes[Main.SETTINGS.theme]["second_color"], activebackground=themes[Main.SETTINGS.theme]["second_color"], relief=RIDGE, \
+            self.close_button = Main.MENU_CANVAS.create_window(Main.MENU_CANVAS.bbox(self.download_button)[2]+12, Main.MENU_CANVAS.bbox(self.update_text_draw)[3]-10, anchor=W, window=Button(image=MyImage.CLOSE, width=16, height=16, bd=0, bg=themes[Main.SETTINGS.theme]["second_color"], activebackground=themes[Main.SETTINGS.theme]["second_color"], relief=RIDGE, \
                 command=lambda: self.close_msg()))
 
     def get_download_url(self):
