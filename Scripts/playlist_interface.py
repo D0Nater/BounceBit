68c673dfa46530ddea86bbc4c7462f3524add771
D0Nater <53537581+D0Nater@users.noreply.github.com>
2020-12-07 19:24:59 +0300

    Update playlist_interface.py

--- Scripts/playlist_interface.py
+++ Scripts/playlist_interface.py
@@ -7,16 +7,18 @@ from Scripts.playlist_storage import PlaylistStorage
 
 class DrawPlaylist:
     def __init__(self, playlist_name, x, y, music_interface):
+        self.music_interface = music_interface
         self.playlist_name = playlist_name
         self.x = x
         self.y = y
 
+    def draw_playlist(self):
         self.playlist_name_text = self.playlist_name[:40]+"..." if len(self.playlist_name) > 40 else self.playlist_name
 
         self.draw_playlist_name = Main.DATA_CANVAS.create_text(self.x, self.y, text=self.playlist_name_text, fill=themes[Main.SETTINGS.theme]["text_color"], anchor=W, font="Verdana 13")
 
         self.button_more = Main.DATA_CANVAS.create_window(Main.DATA_CANVAS.bbox(self.draw_playlist_name)[2]+13, Main.DATA_CANVAS.bbox(self.draw_playlist_name)[3]+2, anchor=SW, window=Button(image=MyImage.MORE, width=20, height=20, bd=0, bg=themes[Main.SETTINGS.theme]["background"], activebackground=themes[Main.SETTINGS.theme]["background"], relief=RIDGE, \
-            command=lambda: music_interface(f"Плейлист {self.playlist_name}", None)))
+            command=lambda: self.music_interface(f"Плейлист {self.playlist_name}", None)))
 
     def set_class(self, playlist_class):
         self.playlist_class = playlist_class
@@ -28,10 +30,7 @@ class DrawPlaylist:
 
         PlaylistStorage.add_playlist("database2.sqlite", self.playlist_name, self.music_data)
 
-        self.draw_playlist_name = Main.DATA_CANVAS.create_text(self.x, self.y, text=self.playlist_name, fill=themes[Main.SETTINGS.theme]["text_color"], anchor=W, font="Verdana 13")
-
-        self.button_more = Main.DATA_CANVAS.create_window(Main.DATA_CANVAS.bbox(self.draw_playlist_name)[2]+13, Main.DATA_CANVAS.bbox(self.draw_playlist_name)[3]+2, anchor=SW, window=Button(image=MyImage.MORE, width=20, height=20, bd=0, bg=themes[Main.SETTINGS.theme]["background"], activebackground=themes[Main.SETTINGS.theme]["background"], relief=RIDGE, \
-            command=lambda: Main.PLAYLIST_INTERFACE.playlist_draw(self.playlist_class, self.playlist_name)))
+        self.draw_playlist()
 
     def delete_playlist(self, music_data):
         self.music_data = music_data
@@ -53,20 +52,27 @@ class DrawPlaylists:
         self.lib_name = lib_name
         self.music_interface = music_interface
 
-    def clear_all(self, draw=True):
-        try:
-            Main.DATA_CANVAS.delete(self.playlist_text)
+        self.x_new_playlist = 0
+        self.y_new_playlist = 0
 
-            Main.DATA_CANVAS.delete(self.set_playlist_name_draw)
-            Main.DATA_CANVAS.delete(self.create_button)
-            Main.DATA_CANVAS.delete(self.cancel_button)
+    def draw_playlists(self, x):
+        # Draw all created playlists #
+        playlists = PlaylistStorage.get_playlists("database2.sqlite")[::-1]
 
-            if draw:
-                self.draw_new_playlist()
-        except AttributeError:
-            pass
+        self.y = Main.DATA_CANVAS.bbox(self.playlist_text)[3]+32
+
+        # Draw playlists #
+        for playlist_name in playlists:
+            new_playlist = DrawPlaylist(playlist_name, x, self.y, self.music_interface)
+            new_playlist.draw_playlist()
+            new_playlist.set_class(new_playlist)
+
+            self.y += 40
 
     def draw_new_playlist(self, x, y):
+        self.x_new_playlist = x
+        self.y_new_playlist = y
+
         # Create playlist text #
         self.playlist_text = Main.DATA_CANVAS.create_text(x, y, text=languages["create_pl"][Main.SETTINGS.language], fill=themes[Main.SETTINGS.theme]["text_color"], anchor=W, font="Verdana 13")
         self.playlist_button = Main.DATA_CANVAS.create_window(Main.DATA_CANVAS.bbox(self.playlist_text)[2]+30, y+1, window=Button(image=MyImage.NEW_PLAYLIST, width=27, height=27, bd=0, bg=themes[Main.SETTINGS.theme]["background"], activebackground=themes[Main.SETTINGS.theme]["background"], relief=RIDGE, \
@@ -82,29 +88,29 @@ class DrawPlaylists:
         PlaylistStorage.add_playlist("database2.sqlite", name)
 
         x = Main.DATA_CANVAS.bbox(self.playlist_text)[0]
-        y = Main.DATA_CANVAS.bbox(self.playlist_text)[3]
+        y = Main.DATA_CANVAS.bbox(self.playlist_text)[3]-7
 
         self.clear_all(draw=False)
 
         new_playlist = DrawPlaylist(name, x, y, self.music_interface)
+        new_playlist.draw_playlist()
         new_playlist.set_class(new_playlist)
 
-    def draw_playlists(self, x):
-        # Draw all created playlists #
-        playlists = PlaylistStorage.get_playlists("database2.sqlite")[::-1]
+    def clear_all(self, draw=True):
+        try:
+            Main.DATA_CANVAS.delete(self.playlist_text)
 
-        self.y = Main.DATA_CANVAS.bbox(self.playlist_text)[3]+32
+            Main.DATA_CANVAS.delete(self.set_playlist_name_draw)
+            Main.DATA_CANVAS.delete(self.create_button)
+            Main.DATA_CANVAS.delete(self.cancel_button)
 
-        # Draw playlists #
-        for playlist_name in playlists:
-            new_playlist = DrawPlaylist(playlist_name, x, self.y, self.music_interface)
-            new_playlist.set_class(new_playlist)
-
-            self.y += 40
+            if draw:
+                self.draw_new_playlist(self.x_new_playlist, self.y_new_playlist)
+        except AttributeError:
+            pass
 
     def draw_create_playlist(self):
         # Clear #
-        Main.PLAYLIST_INTERFACE.close_playlist()
         Main.DATA_CANVAS.delete(self.playlist_text)
         Main.DATA_CANVAS.delete(self.playlist_button)
 
