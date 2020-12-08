5634ee5f9c70c179ffcbe67aa74d25037994b8b1
D0Nater <53537581+D0Nater@users.noreply.github.com>
2020-12-08 15:54:56 +0300

    Update settings_interface.py

--- Scripts/settings_interface.py
+++ Scripts/settings_interface.py
@@ -5,7 +5,6 @@ from pyperclip import copy as copy_text
 
 from Scripts.elements import *
 from Scripts.news import News
-from Scripts.load_bg import LoadBackground
 
 
 class DrawThemeButton:
@@ -16,7 +15,7 @@ class DrawThemeButton:
         return Main.DATA_CANVAS.bbox(self.draw_theme)[2]
 
 
-class SettingsInterface(LoadBackground):
+class SettingsInterface:
     def change_settings(self, setting, new_setting):
         # update settings #
         if setting == "theme":
@@ -79,27 +78,19 @@ class SettingsInterface(LoadBackground):
         x = Main.DATA_CANVAS.bbox(self.theme_text)[2]+20
         for theme_now in themes:
             draw_theme = DrawThemeButton(x, theme_now)
-            x = draw_theme.get_x()+18
-
-        # Background picture #
-        self.bg_text = Main.DATA_CANVAS.create_text(15, 112, text=languages["Фон"][Main.SETTINGS.language], anchor=W, fill=themes[Main.SETTINGS.theme]["text_color"], font="Verdana 13")
-        self.bg_button = Main.DATA_CANVAS.create_window(Main.DATA_CANVAS.bbox(self.bg_text)[2]+20, 113, anchor=W, window=Button(text=languages["load_img"][Main.SETTINGS.language], command=lambda: self.load_background(), bg=themes[Main.SETTINGS.theme]["background"], fg=themes[Main.SETTINGS.theme]["text_color"], bd=1, width=21))
-        self.bg_file = Main.DATA_CANVAS.create_text(Main.DATA_CANVAS.bbox(self.bg_button)[2]+5, 112, text=" - "+Main.SETTINGS.bg_image.split("/")[-1], anchor=W, fill=themes[Main.SETTINGS.theme]["text_color"], font="Verdana 12")
-
-        if Main.SETTINGS.bg_image != "None":
-            self.del_bg = Main.DATA_CANVAS.create_window(Main.DATA_CANVAS.bbox(self.bg_file)[2]+13, 113, window=Button(image=MyImage.TRASHCAN, width=17, height=17, bd=0, bg=themes[Main.SETTINGS.theme]["background"], activebackground=themes[Main.SETTINGS.theme]["background"], command=lambda: self.del_background()), anchor=W)
+            x = draw_theme.get_x()+17
 
         # Languages #
-        self.lang_text = Main.DATA_CANVAS.create_text(15, 160, text=languages["Язык"][Main.SETTINGS.language], anchor=W, fill=themes[Main.SETTINGS.theme]["text_color"], font="Verdana 13")
-        self.ru_lang = Main.DATA_CANVAS.create_window(Main.DATA_CANVAS.bbox(self.lang_text)[2]+20, 161, anchor=W, window=Button(text="Русский", width=15, bd=1, bg=themes[Main.SETTINGS.theme]["background"], fg=themes[Main.SETTINGS.theme]["text_color"], command=lambda: self.change_settings("lang", "ru")))
-        self.en_lang = Main.DATA_CANVAS.create_window(Main.DATA_CANVAS.bbox(self.ru_lang)[2]-1, 161, anchor=W, window=Button(text="English", width=15, bd=1, bg=themes[Main.SETTINGS.theme]["background"], fg=themes[Main.SETTINGS.theme]["text_color"], command=lambda: self.change_settings("lang", "en")))
+        self.lang_text = Main.DATA_CANVAS.create_text(15, 115, text=languages["Язык"][Main.SETTINGS.language], anchor=W, fill=themes[Main.SETTINGS.theme]["text_color"], font="Verdana 13")
+        self.ru_lang = Main.DATA_CANVAS.create_window(Main.DATA_CANVAS.bbox(self.lang_text)[2]+20, Main.DATA_CANVAS.bbox(self.lang_text)[1]-1, anchor=NW, window=Button(text="Русский", width=15, bd=1, bg=themes[Main.SETTINGS.theme]["background"], fg=themes[Main.SETTINGS.theme]["text_color"], command=lambda: self.change_settings("lang", "ru")))
+        self.en_lang = Main.DATA_CANVAS.create_window(Main.DATA_CANVAS.bbox(self.ru_lang)[2]-1, Main.DATA_CANVAS.bbox(self.lang_text)[1]-1, anchor=NW, window=Button(text="English", width=15, bd=1, bg=themes[Main.SETTINGS.theme]["background"], fg=themes[Main.SETTINGS.theme]["text_color"], command=lambda: self.change_settings("lang", "en")))
 
         # More #
-        self.more_text = Main.DATA_CANVAS.create_text(15, 205, text=languages["more"][Main.SETTINGS.language], anchor=W, fill=themes[Main.SETTINGS.theme]["text_color"], font="Verdana 13")
-        self.more_button = Main.DATA_CANVAS.create_window(Main.DATA_CANVAS.bbox(self.more_text)[2]+12, 207, anchor=W, window=Button(image=MyImage.MORE, width=20, height=20, bd=0, bg=themes[Main.SETTINGS.theme]["background"], activebackground=themes[Main.SETTINGS.theme]["background"], command=lambda: Main.MORE_SETTINGS.draw_more()))
+        self.more_text = Main.DATA_CANVAS.create_text(15, 160, text=languages["more"][Main.SETTINGS.language], anchor=W, fill=themes[Main.SETTINGS.theme]["text_color"], font="Verdana 13")
+        self.more_button = Main.DATA_CANVAS.create_window(Main.DATA_CANVAS.bbox(self.more_text)[2]+12, Main.DATA_CANVAS.bbox(self.more_text)[1]+1, anchor=NW, window=Button(image=MyImage.MORE, width=20, height=20, bd=0, bg=themes[Main.SETTINGS.theme]["background"], activebackground=themes[Main.SETTINGS.theme]["background"], command=lambda: Main.MORE_SETTINGS.draw_more()))
 
         # News #
-        self.news_text = Main.DATA_CANVAS.create_text(135, 240, text=languages["Новости"][Main.SETTINGS.language], fill=themes[Main.SETTINGS.theme]["text_color"], font="Verdana 14")
+        self.news_text = Main.DATA_CANVAS.create_text(135, 210, text=languages["Новости"][Main.SETTINGS.language], fill=themes[Main.SETTINGS.theme]["text_color"], font="Verdana 14")
 
         # Creat block news #
         news = News.read_news()
@@ -110,10 +101,12 @@ class SettingsInterface(LoadBackground):
         self.text_news.place(x=15, y=Main.DATA_CANVAS.bbox(self.news_text)[3]+95, width=Main.DATA_CANVAS.winfo_width()/2/2, height=Main.DATA_CANVAS.winfo_height()/2, anchor=NW)
 
         # Copy Button #
-        Main.DATA_CANVAS.create_window(Main.DATA_CANVAS.bbox(self.news_text)[2]+10, 240, window=Button(image=MyImage.COPY, width=19, height=19, bd=0, bg=themes[Main.SETTINGS.theme]["background"], activebackground=themes[Main.SETTINGS.theme]["background"], command=lambda: copy_text(self.text_news.get(1.0, "end-1c"))), anchor=W)
+        Main.DATA_CANVAS.create_window(Main.DATA_CANVAS.bbox(self.news_text)[2]+10, Main.DATA_CANVAS.bbox(self.news_text)[1], anchor=NW, window=Button(image=MyImage.COPY, width=19, height=19, bd=0, bg=themes[Main.SETTINGS.theme]["background"], activebackground=themes[Main.SETTINGS.theme]["background"], command=lambda: copy_text(self.text_news.get(1.0, "end-1c"))))
 
         # Update window #
         Main.ROOT.update()
         Main.DATA_CANVAS.config(scrollregion=Main.DATA_CANVAS.bbox("all"))
 
         Main.MENU.update_buttons()
+
+        draw_just_lines()
