5a99380a17e6c8bb1109a0e30169329637d5da66
D0Nater <53537581+D0Nater@users.noreply.github.com>
2021-01-21 23:09:41 +0300

    Update update_program.py

--- Scripts/update_program.py
+++ Scripts/update_program.py
@@ -4,8 +4,10 @@
 import requests
 import lxml.html
 
+from os import remove, startfile
+
 """ For make message box """
-from tkinter.messagebox import showinfo
+from tkinter.messagebox import showinfo, askyesno
 
 from Scripts.elements import *
 from Scripts.parse_music import parse_data
@@ -15,11 +17,14 @@ class UpdateProgram:
     def __init__(self):
         self.draw_update = False
 
+        self.downloading = False
+
     def search_upd(self):
         try:
             tree = parse_data("https://github.com/D0Nater/BounceBit/blob/main/__init__.py")
 
-            self.new_version = tree.xpath(f'//*[@id="LC5"]/span/text()')[0].split(': ')[-1]
+            self.new_version = tree.xpath('//*[@id="LC5"]/span/text()')[0].split(": ")[-1]
+            self.total_length = int(tree.xpath('//*[@id="LC7"]/span/text()')[0].split(": ")[-1])
 
             if self.new_version != VERSION:
                 self.draw_update = True
@@ -47,20 +52,70 @@ class UpdateProgram:
         tree = parse_data("https://github.com/D0Nater/BounceBit/blob/main/DownloadProgram")
 
         # parse and decode text #
-        return tree.xpath(f'//*[@id="LC1"]/text()')[0]
+        return tree.xpath('//*[@id="LC1"]/text()')[0]
 
     def download_upd(self):
+        def stop_download():
+            self.downloading = False
+
+        def delete_update():
+            try: remove(f"BounceBit_{self.new_version}.exe")
+            except: pass
+
+        self.downloading = True
+
+        Main.MENU_CANVAS.delete(self.close_button)
+
+        self.close_button = Main.MENU_CANVAS.create_window(Main.MENU_CANVAS.bbox(self.download_button)[2]+40, Main.MENU_CANVAS.bbox(self.update_text_draw)[3]-10, anchor=W, window=Button(image=MyImage.CLOSE, width=16, height=16, bd=0, bg=themes[Main.SETTINGS.theme]["second_color"], activebackground=themes[Main.SETTINGS.theme]["second_color"], relief=RIDGE, \
+            command=lambda: stop_download()))
+
+        Main.MENU_CANVAS.delete(self.download_button)
+
+        downloading_num = StringVar()
+        downloading_num.set("0 %")
+
+        draw_downloading_num_label = Label(Main.MENU_CANVAS, textvariable=downloading_num, bg=themes[Main.SETTINGS.theme]["second_color"], fg=themes[Main.SETTINGS.theme]["text_color"], font="Verdana 11")
+
+        self.draw_downloading_num = Main.MENU_CANVAS.create_window(Main.MENU_CANVAS.bbox(self.update_text_draw)[2]+15, Main.MENU_CANVAS.bbox(self.update_text_draw)[3]-11, window=draw_downloading_num_label, anchor=W)
+
         self.__google_file__ = self.get_download_url()
 
-        with open(f"BounceBitInstall_{self.new_version}.exe", "wb") as f:
+        with open(f"BounceBit_{self.new_version}.exe", "wb") as f:
             response = requests.get(self.__google_file__, stream=True)
 
-            for data in response.iter_content(chunk_size=4096):
-                f.write(data)
+            dl = 0
+            for data in response.iter_content(chunk_size=4000):
+                if self.downloading:
+                    f.write(data)
+
+                    dl += len(data)
+                    done = int(100 * dl / self.total_length)
+
+                    if done > 100:
+                        done = 100
+
+                    downloading_num.set(f"{done} %")
+
+                    continue
+
+                break
 
         self.close_msg()
 
-        showinfo(title=f"BounceBit Update {self.new_version}", message="Program was updated!")
+        if self.downloading:
+            answer = askyesno(title=f"BounceBit Update {self.new_version}", message="Program was updated!\nStart program?")
+            if answer:
+                try:
+                    Main.ROOT.quit()
+                    startfile(f"BounceBit_{self.new_version}.exe")
+                except:
+                    showinfo(title="BounceBit", message="Starting Error!")
+            # showinfo(title=f"BounceBit Update {self.new_version}", message="Program was updated!")
+        else:
+            delete_update()
+            showinfo(title=f"BounceBit Update {self.new_version}", message="Program was not updated!")
+
+        self.downloading = False
 
     def update_msg(self):
         if self.draw_update:
@@ -72,5 +127,6 @@ class UpdateProgram:
         Main.MENU_CANVAS.delete(self.update_text_draw)
         Main.MENU_CANVAS.delete(self.download_button)
         Main.MENU_CANVAS.delete(self.close_button)
+        Main.MENU_CANVAS.delete(self.draw_downloading_num)
 
         self.draw_update = False
