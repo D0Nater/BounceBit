334273d1fe442aaf7bada0f60d5b271b44c7a409
D0Nater <53537581+D0Nater@users.noreply.github.com>
2021-01-21 13:36:47 +0300

    Update key_event.py

--- Scripts/key_event.py
+++ Scripts/key_event.py
@@ -6,19 +6,38 @@ from Scripts.main import Main
 
 class KeyEvent:
     def __init__(self):
-        Main.ROOT.bind('<space>', self.play_pause)
+        self.is_unbind_keys = False
 
-        Main.ROOT.bind('<Left>', self.behind_music)
-        Main.ROOT.bind('<Right>', self.after_music)
+        Main.ROOT.bind("<space>", self.play_pause)
 
-        Main.ROOT.bind('<Shift_L>', self.more_info)
-        Main.ROOT.bind('<Shift_R>', self.more_info)
+        Main.ROOT.bind("<Left>", self.behind_music)
+        Main.ROOT.bind("<Right>", self.after_music)
+
+        Main.ROOT.bind("<Shift_L>", self.more_info)
+        Main.ROOT.bind("<Shift_R>", self.more_info)
+
+    def bind_keys(self, event):
+        self.is_unbind_keys = False
+        Main.DATA_CANVAS.focus_set()
+
+        Main.ROOT.unbind("<Button-1>")
+
+    def unbind_keys(self, event):
+        self.is_unbind_keys = True
+
+        Main.ROOT.bind("<Button-1>", self.bind_keys)
 
     def play_pause(self, event):
+        if self.is_unbind_keys:
+            return
+
         if Main.SONG_PLAY_NOW["song_id"]:
             Main.SONG_LINE.click_play()
 
     def more_info(self, event):
+        if self.is_unbind_keys:
+            return
+
         if Main.PAST_SONG["class"] and Main.PAST_SONG["lib_now"] != "Settings":
             if Main.MORE_INFO_INTERFACE.num_of_wins:
                 Main.MORE_INFO_INTERFACE.close_song_info()
@@ -26,9 +45,18 @@ class KeyEvent:
                 Main.MORE_INFO_INTERFACE.song_info_draw(Main.PAST_SONG["class"].song_data)
 
     def behind_music(self, event):
+        if self.is_unbind_keys:
+            return
+
         if Main.SONG_PLAY_NOW["song_id"]:
             Main.SONG_LINE.behind_after_music(-1)
 
     def after_music(self, event):
+        if self.is_unbind_keys:
+            return
+
         if Main.SONG_PLAY_NOW["song_id"]:
             Main.SONG_LINE.behind_after_music(1)
+
+    def other_keys(self, event):
+        print(event)
